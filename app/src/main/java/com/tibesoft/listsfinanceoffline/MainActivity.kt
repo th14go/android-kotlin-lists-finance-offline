50ac857f039ee64b50df478d0c6fb0cd0b210552
Thiago Rodrigues <th14go@mail.com>
2023-08-07 20:44:51 -0300

    view binding configured

--- app/src/main/java/com/tibesoft/listsfinanceoffline/MainActivity.kt
+++ app/src/main/java/com/tibesoft/listsfinanceoffline/MainActivity.kt
@@ -2,10 +2,14 @@ package com.tibesoft.listsfinanceoffline
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tibesoft.listsfinanceoffline.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
